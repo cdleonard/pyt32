c4f47a66100f7691efac14726421f4f38f9316f9
Leonard Crestez <leonard.crestez@nxp.com>
2020-03-25 22:19:13 +0200

    setup.py: Initial commit
    Signed-off-by: Leonard Crestez <leonard.crestez@nxp.com>

---
+++ setup.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+
+from setuptools import setup
+
+setup(
+    name='pyt32',
+    version='0.1',
+    author='Leonard Crestez',
+    author_email='cdleonard@gmail.com',
+    url='https://github.com/cdleonard/pyt32',
+    packages=['t32'],
+)
+
